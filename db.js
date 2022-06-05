d28f44c5b505982c80e132aaa572bf5c515e6700
XIV <xivnick@gmail.com>
2022-06-05 17:31:51 +0900

    add queryformat

--- db.js
+++ db.js
@@ -2,11 +2,22 @@
 const mysql = require('mysql2/promise');
 const db_config = require('./db_config.json');
 
+const queryFormat = (query, values) => {
+	if (!values) return query;
+	return query.replace(/:(\w+)/g, (txt, key) => {
+  		if (Object.prototype.hasOwnProperty.call(values, key)) {
+   			return mysql.escape(values[key]);
+  		}
+  		return txt;
+	});
+}
+
 const pool = mysql.createPool({
 	host: db_config.host,
 	user: db_config.user,
 	password: db_config.password,
 	database: db_config.database,
+	queryFormat,
 });
 
 module.exports = {
